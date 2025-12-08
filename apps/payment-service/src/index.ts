6be46a5a122954fe0090ebd5e38fa8a30aa7fae9
Shivanshu999 <shivanshuawasthi4@gmail.com>
2025-12-08 18:39:01 +0530

    Cleanup repo: remove .next and .turbo from Git

--- apps/payment-service/src/index.ts
+++ apps/payment-service/src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import helmet from 'helmet';
+import  helmet  from "helmet";
 import dotenv from 'dotenv';
 import { config } from './config/env.config.js';
 import { paymentRoutes } from './routes/paymentRoutes.js';
