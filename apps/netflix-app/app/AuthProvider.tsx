1e5e0f04b525895fc48a3732205edffd2af0a05c
Shivanshu999 <shivanshuawasthi4@gmail.com>
2025-12-08 13:29:16 +0530

    type error

--- apps/netflix-app/app/AuthProvider.tsx
+++ apps/netflix-app/app/AuthProvider.tsx
@@ -1,6 +1,9 @@
 "use client";
 import {KindeProvider} from "@kinde-oss/kinde-auth-nextjs";
+interface AuthProviderProps {
+  children: React.ReactNode;
+}
 
-export const AuthProvider = ({children}) => {
+export const AuthProvider = ({children}: AuthProviderProps) => {
   return <KindeProvider>{children}</KindeProvider>;
 };
