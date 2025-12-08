d1a06e6336749f069088a14695db74a06ecd4d03
Shivanshu999 <shivanshuawasthi4@gmail.com>
2025-12-08 16:49:06 +0530

    type

--- apps/netflix-app/next.config.js
+++ apps/netflix-app/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    ignoreDuringBuilds: true, // <-- THIS FIXES YOUR VERCEL BUILD
+    ignoreDuringBuilds: true, // 
   },
 
   images: {
